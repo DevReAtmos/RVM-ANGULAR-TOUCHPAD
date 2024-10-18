6d15e59108334c8dec48bed8e9eff19df3860779
keyur1555 <19bet1030@gmail.com>
2024-10-18 17:43:05 +0530

    COMPLETE

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router,NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,15 +10,33 @@ import { Router } from '@angular/router';
 export class HeaderComponent {
     logo1: string = 'assets/images/logo.png';
     logo2: string = 'assets/images/logo.png';
+    hideImages: boolean = false;
 
 
   constructor(
     private router: Router,
   ){
-
+           
   }
 
-  ngOnInit(){}
+  ngOnInit(){
+    this.updateImageVisibility(this.router.url);
+
+    // Subscribe to router events to detect navigation changes
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(event => {
+        // Type assertion to ensure `event` is of type `NavigationEnd`
+        this.updateImageVisibility((event as NavigationEnd).url);
+      });
+  }
+
+
+  private updateImageVisibility(url: string) {
+    this.hideImages = url === '/password' || url === '/phone';
+  }
+
+
 
   openAdmin(){
     if(this.router.url === '/home' || this.router.url === '/filled-win'){
