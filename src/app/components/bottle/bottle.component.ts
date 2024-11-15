c41bcebbcee27755cbdd9f2ec57db79baf23ae66
tanishWB <tanish.prajapati@wanbuffer.in>
2024-11-15 14:45:13 +0530

    15-11-24

--- src/app/components/bottle/bottle.component.ts
+++ src/app/components/bottle/bottle.component.ts
@@ -227,7 +227,7 @@ export class BottleComponent implements OnInit{
               this.counter = 60;
               this.crush(this.dataID, true, false, false);
             }
-            else if(data.bottleStatus && data.weight > 100){
+            else if(data.bottleStatus && data.weight > 70){
               this.counter = 60;
               this.isCrushing = true;
               this.hideButtons = true;
@@ -276,7 +276,7 @@ export class BottleComponent implements OnInit{
           //   }, 8000);
           // }       
        }   
-      if(data.bottleStatus && data.weight > 100){
+      if(data.bottleStatus && data.weight > 70){
         this.counter = 60;
         this.isCrushing = true;
         this.hideButtons = true;
@@ -297,7 +297,7 @@ export class BottleComponent implements OnInit{
       }
 
      
-      if(data.metal && data.weight > 30){
+      if(data.metal && data.weight > 40){
         this.isCrushing = true;
         this.hideButtons = true;
         this.counter = 60;
